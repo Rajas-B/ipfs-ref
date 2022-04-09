d49e8dc63c029e45e11e6133de5eba486db15bc2
Rajas-B <bondalerajas@gmail.com>
2022-04-09 19:32:55 +0530

    Styled upload form

--- ipfs-get.py
+++ ipfs-get.py
@@ -12,10 +12,11 @@ headers = {
 # resp = requests.post("http://127.0.0.1:5001/api/v0/object/get?arg=QmSau2JosdfFiWpxn2KjZNG55QihfQzGKSaXUcceF5p5iF&encoding=json")
 # resp = requests.post("http://127.0.0.1:5001/api/v0/dag/get?arg=QmSau2JosdfFiWpxn2KjZNG55QihfQzGKSaXUcceF5p5iF&output-codec=dag-json")
 # resp = requests.post("http://127.0.0.1:5001/api/v0/files/read?arg=/ipfs/QmSau2JosdfFiWpxn2KjZNG55QihfQzGKSaXUcceF5p5iF")
-resp = requests.post("http://127.0.0.1:5001/api/v0/cat?arg=QmSau2JosdfFiWpxn2KjZNG55QihfQzGKSaXUcceF5p5iF")
-image_file = io.BytesIO(resp.content)
-image = Image.open(image_file)
-file_path = "./best.png"
-
-with open(file_path, "wb") as f:
-    image.save(f, "png")
+resp = requests.post("http://127.0.0.1:5001/api/v0/cat?arg=QmR7MvAVHsnfnMXUtSVU375uSBoFV2dX1zqNid8j2DtQbT")
+print(resp.content)
+# image_file = io.BytesIO(resp.content)
+# image = Image.open(image_file)
+# file_path = "./best-test.png"
+# 
+# with open(file_path, "wb") as f:
+#     image.save(f, "png")
--- ipfs-request.py
+++ ipfs-request.py
@@ -8,10 +8,10 @@ headers = {
 }
 filepath = "./rajas.JPG"
 with Path(filepath).open("rb") as fp:
-    image_binary=fp.read()
+    rajas_binary=fp.read()
 with Path("./trek.PNG").open("rb") as fp:
-    pic_binary = fp.read()
-resp = requests.post("http://127.0.0.1:5001/api/v0/add", files={"rajas": image_binary, "trek": pic_binary}, params={"wrap-with-directory": "true"})
+    trek_binary = fp.read()
+resp = requests.post("http://127.0.0.1:5001/api/v0/add", files={"rajas": rajas_binary, "trek": trek_binary}, params={"wrap-with-directory": "true"})
 print(resp)
 print(resp.content)
 ipfs_hash=resp.json()["Hash"]
