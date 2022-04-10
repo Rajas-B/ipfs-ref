a56e2479543f0cba99efa87d6ab8bb97e7aa932f
Rajas-B <bondalerajas@gmail.com>
2022-04-10 06:54:10 +0530

    ...

--- ipfs-request.py
+++ ipfs-request.py
@@ -1,20 +1,19 @@
 import requests
 from pathlib import Path
-headers = {
-    "Content-Disposition": "form-data",
-    "name": "file",
-    "filename": "folderName",
-    "Content-Type": "application/x-directory",
-}
+
 filepath = "./rajas.JPG"
 with Path(filepath).open("rb") as fp:
     rajas_binary=fp.read()
 with Path("./trek.PNG").open("rb") as fp:
     trek_binary = fp.read()
-resp = requests.post("http://127.0.0.1:5001/api/v0/add", files={"rajas": rajas_binary, "trek": trek_binary}, params={"wrap-with-directory": "true"})
-print(resp)
+print(type(rajas_binary))
+# resp = requests.post("http://127.0.0.1:5001/api/v0/add", files={"rajas": rajas_binary, "trek": trek_binary}, params={"wrap-with-directory": "true"})
+resp = requests.post("http://127.0.0.1:5001/api/v0/add", files={"rajas": rajas_binary})
+# 
+# print(resp)
 print(resp.content)
 ipfs_hash=resp.json()["Hash"]
-filename=filepath.split("/")[-1:][0]
-image_uri=f"https://ipfs.io/ipfs/{ipfs_hash}?filename={filename}"
-print("image uri on ipfs",image_uri)
+print(ipfs_hash)
+# filename=filepath.split("/")[-1:][0]
+# image_uri=f"https://ipfs.io/ipfs/{ipfs_hash}?filename={filename}"
+# print("image uri on ipfs",image_uri)
